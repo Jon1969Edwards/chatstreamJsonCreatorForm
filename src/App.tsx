e49f2cef1ef1eba48f04c267bf7af4a4da17b584
Jon Edwards <jon1969edwards@gmail.com>
2021-03-22 15:23:38 +0100

    Changes a property to reflect zendesk project

--- src/App.tsx
+++ src/App.tsx
@@ -43,11 +43,11 @@ const useStyles = makeStyles((_theme) => ({
 }));
 
 const initialData = {
-  name: 'Send email to Adrian',
-  description: 'Confirm if you have passed the subject\nHereby ...',
+  name: 'Dick',
   done: true,
-  recurrence: 'Daily',
+  question: 'Is a supportcase already sent?',
   rating: 3,
+  description: 'Is a supportcase already sent?',
 };
 
 const renderers = [
@@ -67,6 +67,9 @@ const App = () => {
   }, [jsonformsData]);
 
   const clearData = () => {
+    setJsonformsData({});
+  };
+  const copyData = () => {
     setJsonformsData({});
   };
 
@@ -116,6 +119,9 @@ const App = () => {
               onChange={({ errors, data }) => setJsonformsData(data)}
             />
           </div>
+          <button onClick={copyData} color='primary'>
+            Copy Data
+          </button>
         </Grid>
       </Grid>
     </Fragment>
